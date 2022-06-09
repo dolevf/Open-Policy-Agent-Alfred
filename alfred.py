a5a15fe9d8ac3ed597507325aee06321c288432a
dolevf <farhi.dolev@gmail.com>
2022-06-09 13:48:15 -0400

    change port

--- alfred.py
+++ alfred.py
@@ -39,4 +39,4 @@ def versions():
     return dict(opa_bin_version=OPA_VERSION, app_version=VERSION)
 
 if __name__ == '__main__':
-    app.run(port=5005, host='0.0.0.0')
+    app.run(port=5000, host='0.0.0.0')
